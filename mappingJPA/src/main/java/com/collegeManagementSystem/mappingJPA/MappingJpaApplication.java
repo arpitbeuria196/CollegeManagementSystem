364c60a870a1c02e1425cff7797900258de9bf45
arpitbeuria196 <arpitbeuria196@gmail.com>
2025-01-26 20:21:23 +0000

    pagination

---
+++ mappingJPA/src/main/java/com/collegeManagementSystem/mappingJPA/MappingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.collegeManagementSystem.mappingJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingJpaApplication.class, args);
+	}
+
+}
